6f6b82092e8286268f523859fd06e7ff01beb41d
mkrooted256 <koreshov.m@gmail.com>
2020-03-01 00:50:43 +0200

    lab1 finished

--- lab1/src/recursion.jl
+++ lab1/src/recursion.jl
@@ -1,27 +1,19 @@
-#=
-main:
-- Julia version: 1.0.5
-- Author: mkrooted
-- Date: 2020-02-03
-=#
-
 r_power(b, p) =
     p == 0 ? 1 : b*r_power(b, p - 1)
 
-r_euclid_w(a, b) =
-    b == 0 ? a : r_euclid_w(b, a % b)
 
 function r_euclid(a, b)
+    r_euclid_w(a, b) =
+        b == 0 ? a : r_euclid_w(b, a % b)
     # Ensure a is greater or equal than b
     if a < b c = a; a = b; b = c; end
     # Call worker
     r_euclid_w(a, b)
 end
 
-r_fibbonaci_w(n) =
-    n < 2 ? 1 : r_fibbonaci_w(n - 1) + r_fibbonaci_w(n - 2)
-
 function r_fibbonaci(n)
+    r_fibbonaci_w(n) =
+        n < 2 ? 1 : r_fibbonaci_w(n - 1) + r_fibbonaci_w(n - 2)
     # Input validation
     n >= 0 || error("n must be non-negative")
     # Worker. Init condition: f_0 = f_1 = 1.
@@ -62,13 +54,15 @@ function r_hanoi(n)
     end
 
     function r_hanoi_w(n, from, mid, to)
-
         if n > 0
             r_hanoi_w(n-1, from, to, mid)
+
             printpegs()
             steps += 1
             println("Step ", steps, ": Move disc ", n, " from ", from.name, " to ", to.name)
+
             push!(to.bar, pop!(from.bar))
+
             r_hanoi_w(n-1, mid, from, to)
         end
     end
